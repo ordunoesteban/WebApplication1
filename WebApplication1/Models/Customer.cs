2077cc3f2d946807b30a9226ec23568f5053af21
Esteban Orduno <ordunoesteban@outlook.com>
2017-10-22 21:38:31 -0500

    Adding a new file

---
+++ WebApplication1/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Customer
+    {
+    }
+}
